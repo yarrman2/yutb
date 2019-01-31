08a22787a163e3d965a0f0b554a06e85752d7f11
Yarrrman <yarrrman@group444.com>
2019-01-31 17:38:52 +0300

    w

---
+++ themeContainer.js
@@ -0,0 +1,82 @@
+class ThemeItem extends Phaser.GameObjects.Container {
+    constructor(scene, x, y, text, val, valText) {
+        super(scene, x, y);
+        var container = this;
+        container.setSize(WIDTH, 248);
+
+        var rect = scene.add.rectangle(40, 20, 208, 248, 0xe5e5e5);
+        rect.setOrigin(0);
+
+        var title = scene.add.text(283, 20, text, {fontFamily: 'Arial', fontSize: 30, color: '#000'});
+
+        var goldContainer = new curSym(scene, 283, 100, val, valText);
+        
+        var shapes = scene.plugins.get('Shapes');
+        var t = scene.add.text(0, 0, 'Принять', {fontFamily:'Arial', fontSize: 25, color: '#fff'});
+        t.setOrigin(0.5)
+        var succBut = shapes.getButton(scene, {x: 283, y: 150, width:300, height:62, radius:6, frontColor: 0x095fd4, activeColor: 0xa0c2ee}, t, function () {})
+        
+        container.add([rect, title, goldContainer, succBut]);
+        return container;
+    }
+}
+
+class ThemeContainer extends Phaser.GameObjects.Container {
+    constructor(scene, x, y) {
+        super(scene, x, y);
+        var container = this;
+        var w = WIDTH;
+        var shapes = scene.plugins.get('Shapes');
+        var player = scene.game.player;
+        var getString =  function (key, curSym, dir, size) {
+            size = size || 20;
+            dir = dir || 1;
+            curSym = curSym || '';
+            return Phaser.Utils.String.Pad(
+                curSym + ' ' + Utils.String.Spaces(player.data.get(key), size, " ", dir
+            ));
+        }
+        scene.children.add(container);
+
+        var bg1 = scene.add.rectangle(0, 0, w, 90, 0xffffff);
+        bg1.setOrigin(0);
+        var leftArrow = scene.add.image(28, 45, 'leftArrow');
+        leftArrow.setOrigin(0, 0.5);
+        var themeTitle = scene.add.text(w/2, 45, 'Тема', {fontFamily: 'Arial', fontSize: 40, color: '#000'});
+        themeTitle.setOrigin(0.5, 0.5);
+        
+        var bg2 = scene.add.rectangle(0, 92, w, 834, 0xffffff);
+        bg2.setOrigin(0);
+
+        container.add([bg2]);
+        
+        var item1 = new ThemeItem(scene, 0, 0, 'Выдавать чужие \nролики за свои', 60, 'в месяц', function () {
+            console.log('item');
+        });
+        
+        var item2 = new ThemeItem(scene, 0, 0, 'Веселые ролики \nс других каналов', 80, 'в месяц', function () {
+            console.log('item');
+        });
+        var item3 = new ThemeItem(scene, 0, 0, 'Веселые ролики \nс других каналов', 160, 'в месяц', function () {
+            console.log('item');
+        });
+        var item4 = new ThemeItem(scene, 0, 0, 'Веселые ролики \nс других каналов', 280, 'в месяц', function () {
+            console.log('item');
+        });
+        var item5 = new ThemeItem(scene, 0, 0, 'Веселые ролики \nс других каналов', 580, 'в месяц', function () {
+            console.log('item');
+        });
+
+        var scrollArea = new ScrollListView(scene, 0, 95, w, 832, container);
+        scrollArea.scrollAdd(item1, 20);
+        scrollArea.scrollAdd(item2, 20);
+        scrollArea.scrollAdd(item3, 20);
+        scrollArea.scrollAdd(item4, 20);
+        scrollArea.scrollAdd(item5, 20);
+        
+        container.add([bg1, leftArrow, themeTitle]);
+        //container.add([scrollArea]);
+
+        return container;
+    }
+}
